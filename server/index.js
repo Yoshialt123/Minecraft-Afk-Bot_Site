48c1458a1230f1f2acb9c4bba2c3558f97037b1c
Yoshialt123 <91681654+Yoshialt123@users.noreply.github.com>
2024-11-18 16:50:48 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const { startBot, stopBot, getBotStatus } = require('./bots/botManager');
+const { startBot, stopBot, getBotStatus } = require('../bots/botManager');
 
 const app = express();
 app.use(express.json());
 
+// Start a bot
 app.post('/start-bot', (req, res) => {
     const { serverName, serverIP, serverPort, username, password, offline } = req.body;
     startBot(serverName, serverIP, serverPort, username, offline === 'on');
     res.status(200).send('Bot started!');
 });
 
+// Stop a bot
 app.post('/stop-bot', (req, res) => {
     const { serverName } = req.body;
     stopBot(serverName);
     res.status(200).send('Bot stopped!');
 });
 
+// Get bot statuses
 app.get('/status', (req, res) => {
     res.json(getBotStatus());
 });
