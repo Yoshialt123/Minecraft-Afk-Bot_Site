c1163affb801ac3d682b2cd4efab154ef8f49586
Yoshialt123 <91681654+Yoshialt123@users.noreply.github.com>
2024-11-18 17:33:19 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { startBot, stopBot, getBotStatus } from './bots/botManager.js'; // Add .js for ES Modules
+import { startBot, stopBot, getBotStatus } from './bots/botManager.js';
 
 const app = express();
 app.use(express.json());
